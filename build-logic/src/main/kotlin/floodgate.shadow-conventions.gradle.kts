bdf06f93e6d87f148514eea64003060521d3f4e4
LCK <1977906158@qq.com>
2026-07-05 07:44:47 +0800

    custom: mark artifacts with mxzc163

--- build-logic/src/main/kotlin/floodgate.shadow-conventions.gradle.kts
+++ build-logic/src/main/kotlin/floodgate.shadow-conventions.gradle.kts
@@ -11,7 +11,7 @@ tasks {
         from(project.rootProject.file("LICENSE"))
     }
     val shadowJar = named<ShadowJar>("shadowJar") {
-        archiveBaseName.set("floodgate-${project.name}")
+        archiveBaseName.set("floodgate-${project.name}-mxzc163")
         archiveVersion.set("")
         archiveClassifier.set("")
 
